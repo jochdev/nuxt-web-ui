4724b3448bedf3a41835bbdbffde9f4bb49ec2f5
José Heredia <hola@joch.dev>
2023-03-04 13:12:44 -0400

    Initial Commit

---
+++ .playground/app.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+const {message} = useHello()
+
+</script>
+
+<template>
+    <div>
+        <h1>{{ message }}</h1>
+        <Button class="bg-red-500">Test!</Button>
+    </div>
+</template>
