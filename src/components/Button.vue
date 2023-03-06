fe01c1cd47929e188dccc5ffef7597e4fdaf9f76
José Heredia <hola@joch.dev>
2023-03-06 10:47:47 -0400

    add ruta tailwind src

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,5 +1,5 @@
 <template>
-    <button>
+    <button class="bg-teal-100 hover:bg-white text-teal-800 rounded-full py-3 px-8 shadow-md hover:shadow-2xl transition duration-500">
         <slot />
     </button>
 </template>
